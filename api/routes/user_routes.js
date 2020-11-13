27f63c866f8d7aad01d46dbbf27b80e0e7947025
Lucien SIGAYRET <luciensigayret>
2020-11-13 12:04:32 +0100

    added paramcheck + db connect

--- api/routes/user_routes.js
+++ api/routes/user_routes.js
@@ -5,11 +5,19 @@ var crypto = require('crypto');
 var secureRandom = require('secure-random');
 const settings = require('../const.js');
 const { MongoClient } = require('mongodb');
+const paramsCheck = require('../src/requiredParams');
+const response = require('../src/response.js');
+
+const client = new MongoClient(settings.api_url, { useUnifiedTopology: true });
 var router = express.Router();
-const client = new MongoClient(settings.api_url);
 
 router.route('/users/register')
-  .post(function (req, res) {
+  .post(async function (req, res) {
+    const check = paramsCheck(req.body, "email", "password");
+    if (!check.success) {
+      res.send(response(401, { error: "Missing parameter", param: check.error }))
+      return 401
+    }
     res.json({
       message: "Create a new user",
       method: req.method
@@ -17,12 +25,21 @@ router.route('/users/register')
   })
 
 router.route('/users/login')
-  .post(function (req, res) {
+  .post(async function (req, res) {
+    const check = paramsCheck(req.body, "email", "password")
+    if (!check.success) {
+      res.send(response(401, { error: "Missing parameter", param: check.error }))
+      return 401;
+    }
+    await client.connect();
+    let dbList = await client.db().admin().listDatabases();
+    console.log("Databases:")
+    dbList.databases.forEach(db => {
+      console.log(`  - ${db.name}`)
+    });
+
     const email = req.body.email;
     const passwd = crypto.createHash('md5').update(req.body.password).digest("hex");
-    await client.connect();
-
-    // Check if user exists and if yes if credentials are ok
 
     const key = secureRandom(256, { type: 'Buffer' });
     const options = {
@@ -31,7 +48,9 @@ router.route('/users/login')
     }
     const jwt_token = jwt.create(options, key);
     jwt_token.setExpiration(new Date().getTime() + 86400 * 1000);
-    res.send(jwt_token.compact());
+    res.send(response(200, { status: "connected", token: jwt_token.compact() }));
+    await client.close();
+    return 200;
   })
 
 router.route('/users/auth/:provider')
