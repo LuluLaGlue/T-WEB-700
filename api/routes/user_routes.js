1189a07077f089ed22fca50005c4fb78c4d8574f
Lucien SIGAYRET <luciensigayret>
2020-11-13 14:01:21 +0100

    added log function + user fetching in log in

--- api/routes/user_routes.js
+++ api/routes/user_routes.js
@@ -6,6 +6,7 @@ const { MongoClient } = require('mongodb');
 const settings = require('../const.js');
 const paramsCheck = require('../src/requiredParams');
 const response = require('../src/response.js');
+const log = require('../src/log.js');
 
 const client = new MongoClient(settings.api_url, { useUnifiedTopology: true });
 var router = express.Router();
@@ -31,35 +32,45 @@ router.route('/users/register')
 router.route('/users/login')
   .post(async function (req, res) {
     if (process.env['USER_ID']) {
+      log("LOGIN", "CHECKING LOGIN STATUS", "USER IS ALREADY LOGGED IN")
       res.status(401).send(response(401, { message: "unauthorized", error: "user can not be logged in" }))
       return 401
     }
     const check = paramsCheck(req.body, "email", "password")
     if (!check.success) {
+      log("LOGIN", "CHECKING PARAMS", "PARAMETER IS MISSING")
       res.status(400).send(response(40, { message: "Missing parameter", param: check.error }))
       return 401;
     }
     await client.connect();
-    let dbList = await client.db().admin().listDatabases();
-    console.log("Databases:")
-    dbList.databases.forEach(db => {
-      console.log(`  - ${db.name}`)
-    });
+    let users = client.db("users");
 
     const email = req.body.email;
     const passwd = crypto.createHash('md5').update(req.body.password).digest("hex");
     //get user based on email + passwd -> process.env['USER_ID'] = id_du_user_connecte
     const key = settings.keysign;
+    const user = await users.collection("users").findOne({
+      email: email,
+      passwd
+    });
+    if (user === null) {
+      log("LOGIN", "CHECKING USER", "USER NOT FOUND")
+      res.status(404).send(response(404, { message: "user not found" }))
+      return 404;
+    }
+    console.log(user)
     const options = {
       iss: "http://cryptoapp.com/",
-      user_id: process.env['USER_ID'] || "user_id",
-      // role: user.role, // Put user role here
+      user_id: user.id,
+      role: user.role, // Put user role here
       sub: passwd
     }
     const jwt_token = jwt.create(options, key);
 
     jwt_token.setExpiration(new Date().getTime() + 86400 * 1000);
+    console.log("Credentials ok")
     res.send(response(200, { message: "connected", token: jwt_token.compact() }));
+    log("LOGIN", "USER LOGGED IN", user)
     await client.close();
     return 200;
   })
---
+++ api/src/log.js
@@ -0,0 +1,7 @@
+function log(func, doing, message) {
+  console.log("FUNCTION: " + func)
+  console.log("DOING: " + doing)
+  console.log("MESSAGE: " + message)
+}
+
+module.exports = log;
