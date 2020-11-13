c2b6b0589c3c3c4b138f6d015690c937a99f1aa8
2O4 <35725720+2O4@users.noreply.github.com>
2020-11-13 15:12:35 +0100

    base avec login/register pour le front token jwt, route privee, components de base

---
+++ the_count_of_money/src/components/admin/admin.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class Admin extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Page admin</h4>
+            </div>
+        );
+    }
+}
+
+export default Admin;
