0cf1c98512f7598bb89e04eeaa2b342a51a1596c
Lucien SIGAYRET <luciensigayret>
2020-11-13 13:25:09 +0100

    added logged in verification

--- api/routes/user_routes.js
+++ api/routes/user_routes.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var jwt = require('njwt');
-const { send } = require('process');
 var crypto = require('crypto');
-var secureRandom = require('secure-random');
-const settings = require('../const.js');
 const { MongoClient } = require('mongodb');
+
+const settings = require('../const.js');
 const paramsCheck = require('../src/requiredParams');
 const response = require('../src/response.js');
 
@@ -13,11 +12,16 @@ var router = express.Router();
 
 router.route('/users/register')
   .post(async function (req, res) {
-    const check = paramsCheck(req.body, "email", "password");
-    if (!check.success) {
-      res.send(response(401, { error: "Missing parameter", param: check.error }))
+    if (process.env['USER_ID']) {
+      res.status(401).send(response(401, { message: "unauthorized", error: "user can not be logged in" }))
       return 401
     }
+    const check = paramsCheck(req.body, "email", "password");
+    if (!check.success) {
+      res.status(400).send(response(400, { message: "Missing parameter", params: check.error }))
+      return 401
+    }
+    await client.connect();
     res.json({
       message: "Create a new user",
       method: req.method
@@ -26,9 +30,13 @@ router.route('/users/register')
 
 router.route('/users/login')
   .post(async function (req, res) {
+    if (process.env['USER_ID']) {
+      res.status(401).send(response(401, { message: "unauthorized", error: "user can not be logged in" }))
+      return 401
+    }
     const check = paramsCheck(req.body, "email", "password")
     if (!check.success) {
-      res.send(response(401, { error: "Missing parameter", param: check.error }))
+      res.status(400).send(response(40, { message: "Missing parameter", param: check.error }))
       return 401;
     }
     await client.connect();
@@ -40,21 +48,28 @@ router.route('/users/login')
 
     const email = req.body.email;
     const passwd = crypto.createHash('md5').update(req.body.password).digest("hex");
-
-    const key = secureRandom(256, { type: 'Buffer' });
+    //get user based on email + passwd -> process.env['USER_ID'] = id_du_user_connecte
+    const key = settings.keysign;
     const options = {
       iss: "http://cryptoapp.com/",
+      user_id: process.env['USER_ID'] || "user_id",
+      // role: user.role, // Put user role here
       sub: passwd
     }
     const jwt_token = jwt.create(options, key);
+
     jwt_token.setExpiration(new Date().getTime() + 86400 * 1000);
-    res.send(response(200, { status: "connected", token: jwt_token.compact() }));
+    res.send(response(200, { message: "connected", token: jwt_token.compact() }));
     await client.close();
     return 200;
   })
 
 router.route('/users/auth/:provider')
   .get(function (req, res) {
+    if (process.env['USER_ID']) {
+      res.status(401).send(response(401, { message: "unauthorized", error: "user can not be logged in" }))
+      return 401
+    }
     res.json({
       message: "Trying to login via " + req.params.provider,
       method: req.method
@@ -63,6 +78,10 @@ router.route('/users/auth/:provider')
 
 router.route('/users/auth/:provider/callback')
   .get(function (req, res) {
+    if (process.env['USER_ID']) {
+      res.status(401).send(response(401, { message: "unauthorized", error: "user can not be logged in" }))
+      return 401
+    }
     res.json({
       message: "Getting info from " + req.params.provider,
       method: req.method
@@ -71,23 +90,76 @@ router.route('/users/auth/:provider/callback')
 
 router.route('/users/logout')
   .post(function (req, res) {
-    res.json({
-      message: "Logging out",
-      method: req.method
-    });
+    if (!process.env['USER_ID']) {
+      res.status(401).send(response(401, { message: "unauthorized", error: "user must be logged in" }))
+      return 401
+    }
+    const token = req.header("authorization")
+    if (token === undefined) {
+      res.status(401).send(response(401, { message: "unauthorized", error: "no token" }))
+      return 401;
+    }
+    jwt.verify(token, settings.keysign, function (err, verifiedJwt) {
+      if (err) {
+        res.status(401).send(response(401, { message: "unauthorized", error: err.message }))
+        return 401;
+      } else {
+        process.env['USER_ID'] = undefined;
+        res.send(response(200, {
+          message: "Logged out",
+        }));
+        //Get info from user with id = verifiedJwt.body.user_id
+      }
+    })
   })
 
 router.route('/users/profile')
-  .get(function (req, res) {
-    res.json({
-      message: "Getting user info",
-      method: req.method
-    });
+  .get(async function (req, res) {
+    if (!process.env['USER_ID']) {
+      res.status(401).send(response(401, { message: "unauthorized", error: "user must be logged in" }))
+      return 401
+    }
+    const token = req.header("authorization")
+    if (token === undefined) {
+      res.status(401).send(response(401, { message: "unauthorized", error: "no token" }))
+      return 401;
+    }
+    jwt.verify(token, settings.keysign, function (err, verifiedJwt) {
+      if (err) {
+        console.log(err)
+        res.status(401).send(response(401, { message: "unauthorized", error: err.message }))
+        return 401;
+      } else {
+        res.send({
+          message: "Getting user info",
+          method: req.method
+        });
+        //Get info from user with id = verifiedJwt.body.user_id
+      }
+    })
   })
   .put(function (req, res) {
-    res.json({
-      message: "updating user info",
-      method: req.method
-    });
+    if (!process.env['USER_ID']) {
+      res.status(401).send(response(401, { message: "unauthorized", error: "user must be logged in" }))
+      return 401
+    }
+    const token = req.header("authorization")
+    if (token === undefined) {
+      res.status(401).send(response(401, { message: "unauthorized", error: "no token" }))
+      return 401;
+    }
+    jwt.verify(token, settings.keysign, function (err, verifiedJwt) {
+      if (err) {
+        res.status(401).send(response(401, { message: "unauthorized", error: err.message }))
+        return 401;
+      } else {
+        res.json({
+          message: "updating user info",
+          method: req.method
+        });
+        //Update info from user with id = verifiedJwt.body.user_id
+      }
+    })
   })
+
 module.exports = router
