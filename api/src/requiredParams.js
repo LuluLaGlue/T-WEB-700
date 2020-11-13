c908303fb03cf866aa3ebee0f7f30ee606777b7d
Lucien SIGAYRET <luciensigayret>
2020-11-13 11:19:13 +0100

    added params check

--- api/src/requiredParams.js
+++ api/src/requiredParams.js
@@ -1,5 +1,10 @@
-function paramsCheck() {
-
+function paramsCheck(body, ...params) {
+  for (let i = 0; i < params.length; i++) {
+    if (!body.hasOwnProperty(params[i])) {
+      return { success: false, error: params[i] }
+    }
+  }
+  return { success: true };
 }
 
-module.exports = paramsCheck();
+module.exports = paramsCheck;
