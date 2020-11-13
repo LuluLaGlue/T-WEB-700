46f42133f1f0bc0f7bc42e9d14cba53d68c72d44
Lucien SIGAYRET <luciensigayret>
2020-11-13 12:05:55 +0100

    now sending all missing params

--- api/src/requiredParams.js
+++ api/src/requiredParams.js
@@ -1,8 +1,12 @@
 function paramsCheck(body, ...params) {
+  let missing = [];
   for (let i = 0; i < params.length; i++) {
     if (!body.hasOwnProperty(params[i])) {
-      return { success: false, error: params[i] }
+      missing.push(params[i]);
     }
+  }
+  if (missing.length !== 0) {
+    return { success: false, error: missing };
   }
   return { success: true };
 }
