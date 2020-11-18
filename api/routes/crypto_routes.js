8666af44087b1ad1447b96fd18577236f18b3b70
Lucien SIGAYRET <luciensigayret@gmail.com>
2020-11-18 08:39:24 +0100

    updated format of crypto routes to match user

--- api/routes/crypto_routes.js
+++ api/routes/crypto_routes.js
@@ -1,39 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
-router.route('/cryptos')
-  .get(function (req, res) {
-    res.json({
-      message: "Accessing cryptos",
-      cmids: req.query.cmids,
-      method: req.method
-    })
+
+router.get("/cryptos", (req, res) => {
+  res.json({
+    message: "Accessing cryptos",
+    cmids: req.query.cmids,
+    method: req.method
   })
-  .post(function (req, res) {
-    res.json({
-      message: "Adding new crypto",
-      method: req.method
-    })
+})
+
+router.post("/cryptos", (req, res) => {
+  res.json({
+    message: "Adding new crypto",
+    method: req.method
   })
-router.route('/cryptos/:cmid')
-  .get(function (req, res) {
-    res.json({
-      message: "Accessing cryptos n" + req.params.cmid,
-      method: req.method
-    })
+})
+
+router.route("/cryptos/:cmid", (req, res) => {
+  res.json({
+    message: "Accessing cryptos n" + req.params.cmid,
+    method: req.method
   })
-  .delete(function (req, res) {
-    res.json({
-      message: "Deleting crypto n" + req.params.cmid,
-      method: req.method
-    })
+})
+
+router.delete("/cryptos/:cmid", (req, res) => {
+  res.json({
+    message: "Deleting crypto n" + req.params.cmid,
+    method: req.method
   })
-router.route('/cryptos/:cmid/history/:period')
-  .get(function (req, res) {
-    res.json({
-      message: "Get cryptos n" + req.params.cmid + " during the period " + req.params.period,
-      method: req.method
-    })
+})
+
+router.get("/cryptos/:cmid/history/:period", (req, res) => {
+  res.json({
+    message: "Get cryptos n" + req.params.cmid + " during the period " + req.params.period,
+    method: req.method
   })
+})
 
 module.exports = router
