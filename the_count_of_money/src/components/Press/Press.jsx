3bdf7ccce024d2e078788e0ee11ce7f6975fea30
Da <sandrine.dasilva@fullsave.com>
2020-11-26 11:22:29 +0100

    [ADD] press component +css

--- the_count_of_money/src/components/Press/Press.jsx
+++ the_count_of_money/src/components/Press/Press.jsx
@@ -1,48 +1,79 @@
 import React, { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
-import { Container, Row, Card, Col, CardDeck } from "react-bootstrap";
+import axios from "axios";
+//import { useHistory } from "react-router-dom";
+import { Card, Button } from "react-bootstrap";
 import "./press.css";
-import { getMoneyCurrency } from "../../actions/adminActions";
 
 const Press = () => {
   const [data, setData] = useState([
     {
       id: 1,
-      title: "yo",
+      title: "title1",
       article:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam mollitia voluptas, quo iusto similique",
     },
     {
       id: 2,
-      title: "well",
+      title: "title2",
       article:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam mollitia voluptas, quo iusto similique",
     },
     {
       id: 3,
-      title: "tchoupi",
+      title: "title3",
       article:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam mollitia voluptas, quo iusto similique",
     },
   ]);
+  const [tag, setTag] = useState();
+  const config = {
+    headers: {
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYjc3NmMyMjI1MmYwNGQwMGQwMGM0ZCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjA1ODU5NTY0LCJleHAiOjE2MDY0NjQzNjR9.VXAvnrf9GSQUhcZ39WARbMrj4CeBHlzp82c-x-nfBMg`,
+    },
+  };
+
+  useEffect(() => {
+    const fetchdata = async () =>
+      await axios
+        .get("http://localhost:3000/articles?params=bitcoin", config)
+        .then((res) => {
+          console.log("res", res.data);
+          setData(res.data);
+          console.log("data", data);
+        });
+    const getKeyWord = async () =>
+      await axios.get(`http://localhost:3000/articles/list/categories`, config)
+        .then;
+    fetchdata();
+    getKeyWord();
+  }, []);
 
   return (
     <>
-      <h2 className="title">Presse</h2>
+      <h2 className="title">What's new ?</h2>
 
-      {/* <MaterialTable /> */}
-      <CardDeck className="CardDesk">
-        {data.map((data) => {
-          return (
-            <Card className="press-card">
-              <Card.Body>
-                <Card.Title>{data.title}</Card.Title>
-                <Card.Text>{data.article}</Card.Text>
-              </Card.Body>
-            </Card>
-          );
-        })}
-      </CardDeck>
+      <div>
+        Search with key word :<input>{tag}</input>
+        <Button onClick={setTag} variant="info">
+          Valider
+        </Button>
+      </div>
+
+      <div className="d-flex justify-content-center">
+        <div className="d-flex justify-content-center press-container">
+          {data.map((data) => {
+            return (
+              <Card className="press-card" key={data.id}>
+                <Card.Body>
+                  <Card.Img variant="top" src={data.img} />
+                  <Card.Title>{data.title}</Card.Title>
+                  <Card.Text>{data.src}</Card.Text>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
--- the_count_of_money/src/components/shared/navbar.component.js
+++ the_count_of_money/src/components/shared/navbar.component.js
@@ -1,23 +1,26 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-
 class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-          <Link to="/" className="navbar-brand">Count The Money</Link>
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand">
+            The Count of the Money
+          </Link>
           <div className="collpase nav-collapse">
             <ul className="navbar-nav mr-auto">
-
               <li className="navbar-item">
-                <Link to="/register" className="nav-link">Register</Link>
+                <Link to="/register" className="nav-link">
+                  Register
+                </Link>
               </li>
               <li className="navbar-item">
-                <Link to="/login" className="nav-link">Login</Link>
+                <Link to="/login" className="nav-link">
+                  Login
+                </Link>
               </li>
-
             </ul>
           </div>
         </div>
