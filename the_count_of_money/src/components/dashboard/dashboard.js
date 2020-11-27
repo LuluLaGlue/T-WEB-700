9dbdfdd99e58df652fee3402f2eed02a33fd8b87
Da <sandrine.dasilva@fullsave.com>
2020-11-27 12:37:30 +0100

    [ADD] dashboard + tab

--- the_count_of_money/src/components/dashboard/dashboard.js
+++ the_count_of_money/src/components/dashboard/dashboard.js
@@ -1,15 +1,12 @@
 import { useState } from "react";
 import UpdateProfile from "./UpdateProfile";
-import { Button } from "react-bootstrap";
 
 const Dashboard = () => {
   const [modalShow, setModalShow] = useState(false);
 
   return (
     <>
-      <Button variant="primary" onClick={() => setModalShow(true)}>
-        Update my profile
-      </Button>
+      {/*  <Press /> */}
 
       <UpdateProfile show={modalShow} onHide={() => setModalShow(false)} />
     </>
--- the_count_of_money/src/components/shared/navbar.component.js
+++ the_count_of_money/src/components/shared/navbar.component.js
@@ -1,29 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import UpdateProfile from "../dashboard/UpdateProfile";
+import { Button, Tab, Tabs } from "react-bootstrap";
+import Dashboard from "../dashboard/dashboard";
+import Home from "../core/home";
 
+const Navbar = () => {
+  let isLogged = true;
+  let name = "toto";
+  const [modalShow, setModalShow] = useState(false);
+  const [key, setKey] = useState("home");
 
-class Navbar extends Component {
-  render() {
-    return (
+  return (
+    <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-          <Link to="/" className="navbar-brand">Count The Money</Link>
+          <Link to="/" className="navbar-brand">
+            The count of the Money
+          </Link>
+
           <div className="collpase nav-collapse">
-            <ul className="navbar-nav mr-auto">
+            {isLogged ? (
+              <ul className="navbar-nav mr-auto">
+                <h5 className="navbar-brand">Bienvenue, {name}</h5>
+                <li className="navbar-item">
+                  <Button variant="primary" onClick={() => setModalShow(true)}>
+                    Update my profile
+                  </Button>
 
-              <li className="navbar-item">
-                <Link to="/register" className="nav-link">Register</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/login" className="nav-link">Login</Link>
-              </li>
-
-            </ul>
+                  <UpdateProfile
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
+                  />
+                </li>
+              </ul>
+            ) : (
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/register" className="nav-link">
+                    Register
+                  </Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/login" className="nav-link">
+                    Login
+                  </Link>
+                </li>
+              </ul>
+            )}
           </div>
         </div>
       </nav>
-    );
-  }
-}
+      <Tabs
+        id="controlled-tab-example"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+      >
+        <Tab eventKey="home" title="Home">
+          <Dashboard />
+        </Tab>
+        <Tab eventKey="profile" title="Profile">
+          <Home />
+        </Tab>
+      </Tabs>
+    </>
+  );
+};
 
 export default Navbar;
