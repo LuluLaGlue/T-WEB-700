83ce356a0417850067cac699cff5917d4e4a1139
dhochberger <dylan.hochberger@etu.unistra.fr>
2020-11-30 06:23:53 +0100

    Changed crypto fetch API + added request routes + added fields and OHLC

--- api/models/crypto.js
+++ api/models/crypto.js
@@ -9,12 +9,19 @@ const CryptoSchema = new Schema({
   id: {
     type: String
   },
+  symbol: {
+    type: String
+  },
   is_authorized: {
     type: Boolean,
     default: false
   },
+  is_requested: {
+    type: Boolean,
+    default: false
+  },
   logo: {
-    type: String,
+    type: {},
     required: false
   },
   actual_price: {
@@ -32,96 +39,86 @@ const CryptoSchema = new Schema({
     required: false,
     default: 0
   },
-  highest_price: {
+  market_cap: {
+    type: Number,
+    required: false,
+    default: 0
+  },
+  circulating_supply: {
     type: Number,
     required: false,
     default: 0
   },
   periods: {
-    _1h: {
-      type: Number,
-      required : false,
-      default: 0
-    },
     _1d: {
       type: Number,
       required : false,
       default: 0
     },
-    _7d: {
-      type: Number,
-      required : false,
-      default: 0
-    },
-    _30d:{
-      type: Number,
-      required : false,
-      default: 0
-    },
-    daily: {
-      opening_price:{
-        type: Number,
+    last_24h: {
+      opening_prices:{
+        type: [Number],
         required: false,
-        default: 0
+        default: [0]
       },
-      highest_price:{
-        type: Number,
+      highest_prices:{
+        type: [Number],
         required: false,
-        default: 0
+        default: [0]
       },
-      lowest_price:{
-        type: Number,
+      lowest_prices:{
+        type: [Number],
         required: false,
-        default: 0
+        default: [0]
       },
-      closing_rate:{
-        type: Number,
+      closing_rates:{
+        type: [Number],
         required: false,
-        default: 0
+        default: [0]
       }
     },
-    hourly: {
-      opening_price:{
-        type: Number,
+    last_week: {
+      opening_prices:{
+        type: [Number],
         required: false,
-        default: 0
+        default: [0]
       },
-      highest_price:{
-        type: Number,
+      highest_prices:{
+        type: [Number],
         required: false,
-        default: 0
+        default: [0]
       },
-      lowest_price:{
-        type: Number,
+      lowest_prices:{
+        type: [Number],
         required: false,
-        default: 0
+        default: [0]
       },
-      closing_rate:{
-        type: Number,
+      closing_rates:{
+        type: [Number],
         required: false,
-        default: 0
+        default: [0]
       }
     },
-    minute: {
-      opening_price:{
-        type: Number,
+    last_month: {
+      opening_prices:{
+        type: [Number],
         required: false,
-        default: 0
+        default: [0]
       },
-      highest_price:{
-        type: Number,
+      highest_prices:{
+        type: [Number],
         required: false,
-        default: 0
+        default: [0]
       },
-      lowest_price:{
-        type: Number,
+      lowest_prices:{
+        type: [Number],
         required: false,
-        default: 0
+        default: [0]
       },
-      closing_rate:{
-        type: Number,
+      closing_rates:{
+        type: [Number],
         required: false,
-        default: 0
+        default: [0]
       }
     },
   }
