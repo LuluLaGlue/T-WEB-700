bbf966cb58baaf21f5eb14a0f48c29665ab87348
2O4 <35725720+2O4@users.noreply.github.com>
2020-12-03 15:04:22 +0100

    fix

--- the_count_of_money/src/components/crypto/listCrypto.jsx
+++ the_count_of_money/src/components/crypto/listCrypto.jsx
@@ -40,23 +40,24 @@ export default class CryptoList extends Component {
         ).then(
             response => {
                 this.setState({ cryptos: response.data });
+                return true
             }
         ).catch(function (error) { console.log(error); })
-        return true
     }
 
     cryptoList() {
-        return this.state.cryptos.map(function (currentCrypto, i) {
+        return this.state.cryptos.list.map(function (currentCrypto, i) {
             return <Crypto crypto={currentCrypto} key={i} />
         })
     }
 
     render() {
+        if (this.state.cryptos.list === undefined) { return null }
         return (
             <div>
                 <h2 className="text-center">Today</h2>
                 <div className="row pt-2">
-                    { this.cryptoList() }
+                    {this.cryptoList()}
                     <div className="col-3 p-2">
                         <Link to={"/add/"} className="card-link">
                             <div className="card text-center crypto-card bg-light">
