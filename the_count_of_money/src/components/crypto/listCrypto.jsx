5b66cbde83ae4fe2fe9a3bc569be130c38e698cd
2O4 <35725720+2O4@users.noreply.github.com>
2020-12-11 14:46:07 +0100

    changement de l'affichage de la liste des cryptos

--- the_count_of_money/src/components/crypto/listCrypto.jsx
+++ the_count_of_money/src/components/crypto/listCrypto.jsx
@@ -10,9 +10,21 @@ import { useEffect } from 'react';
 
 
 function Crypto_row(props) {
+    // Props representant une ligne de la liste des cryptos
+    const last_7d = props.crypto.periods.last_week.opening_prices
+    console.log(last_7d[0] - last_7d[last_7d.length - 1])
+    const last_7d_purcent = (
+        (last_7d[last_7d.length - 1] - last_7d[0]) / last_7d[last_7d.length - 1]
+    ) * 100
+
     useEffect(() => {
         const ctx = document.getElementById(props.crypto.rank);
+
+        // recuperation des donnees pour le graph
         const data_list = props.crypto.periods.last_month.opening_prices;
+
+        // utiliser pour changer la couleur du graph selon l'evolution
+        const evolution_price = data_list[data_list.length - 1] - data_list[0]
         new Chart(ctx, {
             type: "line",
             data: {
@@ -22,10 +34,12 @@ function Crypto_row(props) {
                     pointRadius: 0,
                     fill: false,
                     lineTension: 0,
+                    borderColor: evolution_price > 0 ? '#22922d' : '#dc3545',
+                    borderWidth: 2,
                 }],
             },
             options: {
-                responsive:true,
+                responsive: true,
                 maintainAspectRatio: false,
                 legend: {
                     display: false
@@ -52,7 +66,7 @@ function Crypto_row(props) {
                             drawTicks: false,
                         },
                         ticks: {
-                            callback: function(value, index, values) {
+                            callback: function (value, index, values) {
                                 return '$' + value;
                             },
                             display: false,
@@ -64,7 +78,7 @@ function Crypto_row(props) {
     });
 
     return (
-        <tr class="list-group-item-action">
+        <tr class="list-group-item-action border-bottom">
             <td class="align-middle font-weight-bold">{props.crypto.rank}</td>
             <th class="align-middle py-4" scope="row">
                 <Link to={"/detail/" + props.crypto.id} className="text-body text-decoration-none">
@@ -72,14 +86,35 @@ function Crypto_row(props) {
                     {props.crypto.name} <span class="text-muted font-weight-normal">{props.crypto.symbol}</span>
                 </Link>
             </th>
-            <td class="align-middle">€{new Intl.NumberFormat().format(props.crypto.actual_price.toFixed(4))}</td>
-            <td class={props.crypto.price_change_24h > 0 ? 'text-success align-middle' : 'text-danger align-middle'}>
-                <span id="caret">{props.crypto.price_change_24h > 0 ? '▲' : '▼'}</span>
-                {props.crypto.price_change_24h.toFixed(2)}%
+            <td class="align-middle">
+                <span class="d-flex justify-content-end align-items-center">
+                    <b>€{new Intl.NumberFormat().format(props.crypto.actual_price.toFixed(4))}</b>
+                </span>
             </td>
-            <td class="align-middle">€{new Intl.NumberFormat().format(props.crypto.market_cap.toFixed(0))}</td>
-            <td class="align-middle">{new Intl.NumberFormat().format(props.crypto.circulating_supply.toFixed(0))} {props.crypto.symbol}</td>
-            <td class="chart-container">
+            <td class={props.crypto.price_change_24h > 0 ? 'text-success align-middle' : 'text-danger align-middle'}>
+                <span class="d-flex justify-content-end align-items-center">
+                    <span id="caret">{props.crypto.price_change_24h > 0 ? '▲' : '▼'}</span>
+                    {props.crypto.price_change_24h.toFixed(2)}%
+                </span>
+            </td>
+            <td class={last_7d_purcent > 0 ? 'text-success align-middle' : 'text-danger align-middle'}>
+                <span class="d-flex justify-content-end align-items-center">
+                    <span id="caret">{last_7d_purcent > 0 ? '▲' : '▼'}</span>
+                    {last_7d_purcent.toFixed(2)}%
+                </span>
+            </td>
+            <td class="align-middle">
+                <span class="d-flex justify-content-end align-items-center">
+                    €{new Intl.NumberFormat().format(props.crypto.market_cap.toFixed(0))}
+                </span>
+            </td>
+            <td class="align-middle">
+                <span class="d-flex justify-content-end align-items-center">
+                    {new Intl.NumberFormat().format(props.crypto.circulating_supply.toFixed(0))}
+                    {props.crypto.symbol}
+                </span>
+            </td>
+            <td class="chart-container py-1">
                 <canvas id={props.crypto.rank} width="200" height="60"></canvas>
             </td>
         </tr>
@@ -113,7 +148,6 @@ export default class CryptoList extends Component {
         });
     }
 
-
     render() {
         if (this.state.cryptos.list === undefined) { return null }
 
@@ -134,19 +168,24 @@ export default class CryptoList extends Component {
                             <tr>
                                 <th scope="col">#</th>
                                 <th scope="col">Cryptocurrency</th>
-                                <th scope="col">Price</th>
-                                <th scope="col">24h</th>
+                                <th scope="col"><span class="d-flex justify-content-end align-items-center">Price</span></th>
+                                <th scope="col"><span class="d-flex justify-content-end align-items-center">24h</span></th>
+                                <th scope="col"><span class="d-flex justify-content-end align-items-center">7d</span></th>
                                 <th scope="col">
-                                    <OverlayTrigger placement="bottom" overlay={marketTooltip}>
-                                        <span>Market cap <FontAwesomeIcon className="text-muted" icon={faInfoCircle} /></span>
-                                    </OverlayTrigger>
+                                    <span class="d-flex justify-content-end align-items-center">
+                                        <OverlayTrigger placement="bottom" overlay={marketTooltip}>
+                                            <span>Market cap <FontAwesomeIcon className="text-muted pl-1" icon={faInfoCircle} /></span>
+                                        </OverlayTrigger>
+                                    </span>
                                 </th>
                                 <th scope="col">
-                                    <OverlayTrigger placement="bottom" overlay={supplyTooltip}>
-                                        <span>Supply <FontAwesomeIcon className="text-muted" icon={faInfoCircle} /></span>
-                                    </OverlayTrigger>
+                                    <span class="d-flex justify-content-end align-items-center">
+                                        <OverlayTrigger placement="bottom" overlay={supplyTooltip}>
+                                            <span>Supply <FontAwesomeIcon className="text-muted pl-1" icon={faInfoCircle} /></span>
+                                        </OverlayTrigger>
+                                    </span>
                                 </th>
-                                <th scope="col">Last month</th>
+                                <th scope="col"><span class="d-flex justify-content-end align-items-center">Last month</span></th>
                             </tr>
                         </thead>
                         <tbody>
